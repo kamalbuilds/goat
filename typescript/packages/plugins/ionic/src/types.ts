260a76e0183a278a61c873ddcbc5743b59d1b2e7
kamalbuilds <geniusamansingh@gmail.com>
2024-12-20 07:48:38 +0530

    export supply and borrow position

--- typescript/packages/plugins/ionic/src/types.ts
+++ typescript/packages/plugins/ionic/src/types.ts
@@ -16,7 +16,22 @@ export interface LoopingConfig {
   minCollateralUSD: number;
 }
 
+export interface SupplyPosition {
+  asset: string;
+  amount: string;
+  value: string;
+  apy: number;
+}
+
 export interface CollateralSwapConfig {
   slippageTolerance: number;
   maxSwapSize: number;
+} 
+
+export interface BorrowPosition {
+  asset: string;
+  amount: string;
+  value: string;
+  apy: number;
+  collateral: boolean;
 } 
