9c5623d7700210bc3e562b2af87b841508aa58dd
kamalbuilds <geniusamansingh@gmail.com>
2025-02-07 02:23:13 +0530

    add the sei chain to the config

--- typescript/packages/core/src/types/Chain.ts
+++ typescript/packages/core/src/types/Chain.ts
@@ -12,7 +12,8 @@ export type Chain =
     | ZilliqaChain
     | CosmosChain
     | StarknetChain
-    | RadixChain;
+    | RadixChain
+    | SeiChain;
 
 export type SuiChain = {
     type: "sui";
@@ -56,4 +57,8 @@ export type StarknetChain = {
 export type RadixChain = {
     type: "radix";
     id: number;
+};
+
+export type SeiChain = {
+    type: "sei";
 };
